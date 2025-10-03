5f49fc3f37b77ac3f5e012fc796b897fad243d2c
KerinR432 <kerinaguilera@gmail.com>
2025-10-03 14:15:52 +0200

    Continuar en casa

--- src/main/java/Halo/unsc/Halo/HaloApplication.java
+++ src/main/java/Halo/unsc/Halo/HaloApplication.java
@@ -2,12 +2,22 @@ package Halo.unsc.Halo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@Controller
+@ResponseBody
 public class HaloApplication {
-
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 		SpringApplication.run(HaloApplication.class, args);
 	}
-
+    @GetMapping("/hola")
+    public String hola(@RequestParam(value = "nombre",defaultValue = "Mundo") String nombre){
+        return String.format("Hola %s!", nombre);
+    }
 }
+
